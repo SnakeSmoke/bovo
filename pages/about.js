9fc53738e7e1109b4c3345770552b077b96bc0c2
SnakeSmoke <ormos@live.se>
2021-10-24 21:59:05 +0200

    init commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi dolorum aperiam sint quia, eligendi vitae, eveniet provident odio nulla id rerum, eius ipsa consequuntur molestias perspiciatis quibusdam velit reprehenderit dolore. Adipisci fugit suscipit doloremque sunt minima aspernatur quidem inventore placeat, possimus reiciendis tempora at aliquid consectetur sapiente cumque eum quia est natus vel eaque minus repellat? Reiciendis laboriosam rem autem, pariatur optio mollitia nihil harum perspiciatis sequi inventore excepturi, ex quos qui asperiores repellat alias delectus sit eaque iste aliquam quisquam dolore libero et magni. Officia iusto voluptatum doloribus dignissimos nisi, consequatur magnam, quos sit, dolorum tempore saepe reiciendis. Nemo?
+            </p>
+        </div>
+    )
+}
+
+export default About
